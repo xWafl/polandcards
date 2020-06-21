46d647b31f4b5bfa0cd2ed7792ae82ba59212c98
xWafl <73sampleperson@gmail.com>
2020-06-21 16:55:08 -0400

    :sparkles: Board class beginnings

---
+++ src/modules/game/actions/board.ts
@@ -0,0 +1,30 @@
+class Board {
+    public readonly player1: Card[] = Array(4);
+    public readonly player2: Card[] = Array(4);
+    public player1health = 30;
+    public player2health = 30;
+    public turnNum = 0;
+    public player1Move = true;
+    constructor () {}
+    public playCard (player: number, card: Card, slot: number) {
+        if (player === 1 || player === 2) {
+            const arr = player === 1 ? this.player1 : this.player2;
+            if (!arr[slot]) {
+                arr[slot] = card;
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            return false;
+        }
+    }
+    public endTurn () {
+        if (this.player1Move) {
+            this.player1Move = false;
+        } else {
+            this.player1Move = true;
+            this.turnNum++;
+        }
+    }
+}
