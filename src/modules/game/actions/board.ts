cf7503227ab65aada83008882ccaf67794d9bd0d
xWafl <73sampleperson@gmail.com>
2020-06-21 17:22:52 -0400

    :sparkles: Play and attack cards

--- src/modules/game/actions/board.ts
+++ src/modules/game/actions/board.ts
@@ -5,7 +5,10 @@ class Board {
     public player2health = 30;
     public turnNum = 0;
     public player1Move = true;
-    constructor () {}
+
+    constructor () {
+    }
+
     public playCard (player: number, card: Card, slot: number) {
         if (player === 1 || player === 2) {
             const arr = player === 1 ? this.player1 : this.player2;
@@ -19,6 +22,26 @@ class Board {
             return false;
         }
     }
+
+    public attack (attackerId: number, receiverId: number) {
+        const attackerCards = this.player1Move ? this.player1 : this.player2;
+        const receiverCards = !this.player1Move ? this.player1 : this.player2;
+        const attackerCard = attackerCards.find(l => l.id === attackerId);
+        const receiverCard = receiverCards.find(l => l.id === receiverId);
+        if (!(attackerCard && receiverCard)) {
+            return false;
+        }
+        if (attackerId < 2) {
+            return false;
+        }
+        if (receiverId < 2) {
+            return false;
+        }
+        attackerCard.health -= receiverCard.attack;
+        receiverCard.health -= attackerCard.attack;
+        return true;
+    }
+
     public endTurn () {
         if (this.player1Move) {
             this.player1Move = false;
