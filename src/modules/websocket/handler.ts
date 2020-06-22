c19efd7d7f5b9d82c74621aecb96bf14dbd95c10
xWafl <73sampleperson@gmail.com>
2020-06-22 12:52:26 -0400

    :art: Formatting

--- src/modules/websocket/handler.ts
+++ src/modules/websocket/handler.ts
@@ -1,3 +1,4 @@
 export const websocketHandler = (str: string) => JSON.parse(str);
 
-export const sendSocket = (category: string, data: unknown) => JSON.stringify({ category, data });
+export const sendSocket = (category: string, data: unknown) =>
+    JSON.stringify({ category, data });
