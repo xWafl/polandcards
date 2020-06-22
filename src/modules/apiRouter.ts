854943a49e8e09310589c949ece0a15ade37672d
xWafl <73sampleperson@gmail.com>
2020-06-22 08:20:39 -0400

    :wrench: Router

--- src/modules/apiRouter.ts
+++ src/modules/apiRouter.ts
@@ -1,9 +1,11 @@
 import Router from "koa-router";
 
 import greetingsRouter from "./greetings/routes/";
+import gamesRouter from "./game/router";
 
 const apiRouter = new Router({ prefix: "/api" });
 
 apiRouter.use(greetingsRouter);
+apiRouter.use(gamesRouter);
 
 export default apiRouter.routes();
