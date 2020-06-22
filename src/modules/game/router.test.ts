c51df8bd899d738fa732ca92daa3ff07e038fd5f
xWafl <73sampleperson@gmail.com>
2020-06-22 08:20:29 -0400

    :white_check_mark: Game router testing

---
+++ src/modules/game/router.test.ts
@@ -0,0 +1,17 @@
+import request from "supertest";
+import { expect } from "chai";
+import { server } from "../../index";
+import { games } from "./games";
+import { Board } from "./actions/board";
+
+describe("Root route", () => {
+    it("Creates a game", async () => {
+        await request(server)
+            .post("/api/game/createGame")
+            .set("Accept", "application/json")
+            .expect("Content-Type", /text/)
+            .expect(201);
+
+        expect(games[0]).to.deep.equal(new Board());
+    });
+});
