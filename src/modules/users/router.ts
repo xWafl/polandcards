cc9dc5a02ba936ddd76a034f6c30c9d3c3df0148
xWafl <73sampleperson@gmail.com>
2020-06-23 09:02:39 -0400

    :sparkles: Basic users routes

---
+++ src/modules/users/router.ts
@@ -0,0 +1,30 @@
+import Router from "../Router";
+
+import { createUser } from "./actions/createUser";
+import { HttpError } from "../../common/error/classes/httpError";
+import findUser from "./actions/findUser";
+import { registerBody } from "./schema/registerBody";
+import { validateSchema } from "../schema/middleware/validateSchema";
+import { RegisterBody } from "./types/RegisterBody";
+const router = new Router({ prefix: "/users" });
+
+router.post(
+    "/createUser",
+    validateSchema(registerBody, "body"),
+    async (ctx, next) => {
+        const { username, password, email } = ctx.request.body as RegisterBody;
+
+        const user = await createUser({ email, username, password });
+
+        if (!user) {
+            throw new HttpError(400, "That username seems to be already taken");
+        }
+
+        ctx.session!.user = user.id;
+        ctx.status = 201;
+        ctx.body = { status: 201, message: "Successfully created" };
+        await next();
+    }
+);
+
+export default router.routes();
